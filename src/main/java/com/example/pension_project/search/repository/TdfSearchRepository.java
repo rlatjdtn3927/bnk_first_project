78b7f25687a52a61514a8d2a4257444fe8c6821f
sujiinnn <sujiinnn@gmail.com>
2025-07-16 16:14:06 +0900

    통합검색 api 구현

---
+++ src/main/java/com/example/pension_project/search/repository/TdfSearchRepository.java
@@ -0,0 +1,14 @@
+package com.example.pension_project.search.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.pension_project.jpa.entity.commodity.TDFEntity;
+
+public interface TdfSearchRepository extends JpaRepository<TDFEntity, String> {
+    @Query(value = "SELECT * FROM tdf WHERE CONTAINS(prod_name, :keyword) > 0", nativeQuery = true)
+    List<TDFEntity> searchByKeyword(@Param("keyword") String keyword);
+}
